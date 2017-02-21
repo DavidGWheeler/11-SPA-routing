e1cfae1341bf3042d1d88cdef540e5206ad72cc1
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-02-21 10:41:59 -0800

    add constring for ubuntu

--- starter-code/server.js
+++ starter-code/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = 'postgress://david:Password@localhost:5432/postgress'; //[x] DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect(console.error);
 
